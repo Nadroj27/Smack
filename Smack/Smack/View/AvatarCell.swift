be3ac13927854882e7b612a0f4df36144d226fd6
Jordan Vacca <Jordan@jordans-macbook-pro-2.home>
2018-01-09 01:07:44 +0100

    [+] Avatar Picker VC

---
+++ Smack/Smack/View/AvatarCell.swift
@@ -0,0 +1,30 @@
+//
+//  AvatarCell.swift
+//  Smack
+//
+//  Created by Jordan Vacca on 09/01/2018.
+//  Copyright © 2018 Jordan Vacca. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    
+    override func awakeFromNib()
+    {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    
+    func setUpView()
+    {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+    
+}
